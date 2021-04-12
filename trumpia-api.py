1d22299aefa99cea17aa514f46abbba59a393eb6
Yash <yash_b@csu.fullerton.edu>
2021-04-12 12:04:36 -0700

    logging results

--- trumpia-api.py
+++ trumpia-api.py
@@ -1,6 +1,6 @@
 from flask import Flask, request
 import os
-# from sys import stderr
+from sys import stderr
 import xml.etree.ElementTree as ET
 import socket
 
@@ -8,22 +8,25 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
+    print('home', file=stderr)
     return '200 ok'
 
 @app.route('/inbound/', methods=['GET', 'POST'])
 def inbound():
+    print('inbound', file=stderr)
     if request.method == 'POST':
         return '200 ok'
 
     if request.method == 'GET':
         if 'xml' not in request.args: 
+            return 'NO XML'
             pass
         else:
             response = request.args.get('xml')
             root = ET.fromstring(response)
             for child in root:
                 print(str(child.tag) + ': ' + str(child.text))
-        return ''
+        return 'GET'
 
 
 if __name__ == '__main__':
@@ -33,4 +36,4 @@ if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', debug=True, port=port)
     # print(port)
-    sock.close()
+    # sock.close()
