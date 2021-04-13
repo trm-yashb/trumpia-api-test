998423b505ee503e7728cda24bccef6b4aa8bca1
Yash <yash_b@csu.fullerton.edu>
2021-04-12 22:22:17 -0700

    bug fixes

--- app.py
+++ app.py
@@ -2,8 +2,7 @@ from flask import Flask, request
 import os
 from sys import stderr
 import xml.etree.ElementTree as ET
-import socket
-from flask_ngrok import run_with_ngrok
+# from flask_ngrok import run_with_ngrok
 
 app = Flask(__name__)
 
@@ -33,10 +32,7 @@ def inbound():
 
 
 if __name__ == '__main__':
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.bind(('localhost', 0))
-    port = sock.getsockname()[1]
-    # port = int(os.environ.get('PORT', 80))
-    app.run(debug=True, port=8080)
+    port = int(os.environ.get('PORT', 80))
+    app.run(host='0.0.0.0', debug=True, port=8080)
     # print(port)
-    sock.close()
+    # sock.close()
